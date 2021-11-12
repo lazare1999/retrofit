6bda65ecec711257e78216d938370000f585cea1
Lazare Kvirtia <lazarekvirtia@gmail.com>
2021-11-12 20:38:53 +0400

    quiz_6

--- app/src/main/java/ge/msda/myapplication/App.java
+++ app/src/main/java/ge/msda/myapplication/App.java
@@ -3,12 +3,14 @@ package ge.msda.myapplication;
 import android.app.Application;
 
 import ge.msda.myapplication.api.RetrofitClient;
+import ge.msda.myapplication.api.RetrofitClient2;
 
 public class App extends Application {
 
     private static App instance;
 
     private RetrofitClient retrofitClient;
+    private RetrofitClient2 retrofitClient2;
 
     @Override
     public void onCreate() {
@@ -17,6 +19,7 @@ public class App extends Application {
         instance = this;
 
         retrofitClient = new RetrofitClient();
+        retrofitClient2 = new RetrofitClient2();
 
     }
 
@@ -27,5 +30,10 @@ public class App extends Application {
     public RetrofitClient getRetrofitClient() {
         return retrofitClient;
     }
+
+    public RetrofitClient2 getRetrofitClient2() {
+        return retrofitClient2;
+    }
+
 
 }
--- app/src/main/java/ge/msda/myapplication/MainActivity.java
+++ app/src/main/java/ge/msda/myapplication/MainActivity.java
@@ -8,8 +8,10 @@ import android.util.Log;
 import java.util.List;
 
 import ge.msda.myapplication.api.endpoints.ApiService;
+import ge.msda.myapplication.api.endpoints.ApiService2;
 import ge.msda.myapplication.api.models.Data;
 import ge.msda.myapplication.api.models.PostUserResponseModel;
+import ge.msda.myapplication.api.models.Posts;
 import ge.msda.myapplication.api.models.Resource;
 import ge.msda.myapplication.api.models.User;
 import retrofit2.Call;
@@ -24,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ApiService apiService = App.getInstance().getRetrofitClient().getApiService();
+        ApiService2 apiService2 = App.getInstance().getRetrofitClient2().getApiService();
+
+
+        apiService2.getPosts(1).enqueue(new Callback<Data<List<Posts>>>() {
+
+            @Override
+            public void onResponse(Call<Data<List<Posts>>> call, Response<Data<List<Posts>>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<Posts> users = response.body().getData();
+                    for (int i = 0; i < users.size(); i++) {
+                        Log.d("Posts", users.get(i).toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Data<List<Posts>>> call, Throwable t) {
+
+            }
+
+        });
 
         apiService.getUsers(1).enqueue(new Callback<Data<List<User>>>() {
 
